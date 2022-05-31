1ca0b6a00ec7d3c697d13547a1e64c2d639b7afe
Jeff Robertson <jrobertson@hex.tech>
2022-05-31 18:46:57 -0400

    downgrade aws

--- versions.tf
+++ versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_version = ">= 0.15"
 
   required_providers {
-    aws    = "~> 4.0"
+    aws    = "~> 3.0"
     random = ">= 2.1"
     time   = "~> 0.6"
     lacework = {
       source  = "lacework/lacework"
-      version = "~> 0.17"
+      version = "~> 0.14"
     }
   }
 }
